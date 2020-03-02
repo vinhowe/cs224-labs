a6e73438aaab712dc38f6752dbd90bc1c5b7cb4f
Eric Mercer <egm@cs.byu.edu>
2020-03-02 12:53:59 -0700

    Add first draft of y86-64

---
+++ y86-64/y86-64.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+
+const int MAX_MEM_SIZE  = (1 << 13);
+
+void fetchStage(int *icode, int *ifun, int *rA, int *rB, wordType *valC, wordType *valP) {
+ 
+}
+
+void decodeStage(int icode, int rA, int rB, wordType *valA, wordType *valB) {
+ 
+}
+
+void executeStage(int icode, int ifun, wordType valA, wordType valB, wordType valC, wordType *valE, bool *Cnd) {
+  
+}
+
+void memoryStage(int icode, wordType valA, wordType valP, wordType valE, wordType *valM) {
+ 
+}
+
+void writebackStage(int icode, wordType rA, wordType rB, wordType valE, wordType valM) {
+ 
+}
+
+void pcUpdateStage(int icode, wordType valC, wordType valP, bool Cnd, wordType valM) {
+  
+}
+
+void stepMachine(int stepMode) {
+  /* FETCH STAGE */
+  int icode = 0, ifun = 0;
+  int rA = 0, rB = 0;
+  wordType valC = 0;
+  wordType valP = 0;
+ 
+  /* DECODE STAGE */
+  wordType valA = 0;
+  wordType valB = 0;
+
+  /* EXECUTE STAGE */
+  wordType valE = 0;
+  bool Cnd = 0;
+
+  /* MEMORY STAGE */
+  wordType valM = 0;
+
+  fetchStage(&icode, &ifun, &rA, &rB, &valC, &valP);
+  applyStageStepMode(stepMode, "Fetch", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+
+  decodeStage(icode, rA, rB, &valA, &valB);
+  applyStageStepMode(stepMode, "Decode", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+  
+  executeStage(icode, ifun, valA, valB, valC, &valE, &Cnd);
+  applyStageStepMode(stepMode, "Execute", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+  
+  memoryStage(icode, valA, valP, valE, &valM);
+  applyStageStepMode(stepMode, "Memory", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+  
+  writebackStage(icode, rA, rB, valE, valM);
+  applyStageStepMode(stepMode, "Writeback", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+  
+  pcUpdateStage(icode, valC, valP, Cnd, valM);
+  applyStageStepMode(stepMode, "PC update", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+
+  incrementCycleCounter();
+}
+
+/** 
+ * main
+ * */
+int main(int argc, char **argv) {
+  int stepMode = 0;
+  FILE *input = parseCommandLine(argc, argv, &stepMode);
+
+  initializeMemory(MAX_MEM_SIZE);
+  initializeRegisters();
+  loadMemory(input);
+
+  applyStepMode(stepMode);
+  while (getStatus() != STAT_HLT) {
+    stepMachine(stepMode);
+    applyStepMode(stepMode);
+#ifdef DEBUG
+    printMachineState();
+    printf("\n");
+#endif
+  }
+  printMachineState();
+  return 0;
+}
